628afd6ab895c5bfe26d4644e4f1a8c7b8f91aa0
hoemin-dev <hoemin.dev@outlook.com>
2026-08-25 17:30:43 +0900

    allow authenticated profile commands

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,21 @@
 fn main() {
-  tauri_build::build()
+    tauri_build::try_build(
+        tauri_build::Attributes::new().app_manifest(
+            tauri_build::AppManifest::new()
+                .commands(&[
+                    "notify_login_page_ready",
+                    "close_login_window",
+                    "minimize_login_window",
+                    "begin_access_login",
+                    "log_login_viewport",
+                    "log_login_diagnostic",
+                    "show_login_window",
+                    "toggle_profile_popup",
+                    "hide_profile_popup",
+                    "confirm_access_logout",
+                    "begin_access_logout",
+                ]),
+        ),
+    )
+    .expect("failed to build Tauri application manifest");
 }
